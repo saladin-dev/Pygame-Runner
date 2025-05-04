00bd763e8879e2a90ab7b963677dda367c271414
Saladin Dev <Saladin.Omer@outlook.com>
2025-05-04 16:43:06 -0400

    Added Player (Non-functional)

--- runner.py
+++ runner.py
@@ -4,9 +4,19 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
+
+
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
+
+Player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+Player_rect = Player_surface.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -15,8 +25,15 @@ while True:
             exit()
    
    
-    screen.blit(test_surface, (200, 100))     
-   
+    screen.blit(sky_surface, (0,0))    
+    screen.blit(ground_surface, (0,300)) 
+    screen.blit(text_surface, (300,50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos,265))
+    screen.blit(Player_surface, Player_rect)
+    
    
     pygame.display.update()
     clock.tick(60)
